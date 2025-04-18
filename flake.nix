5c4fe23fe1103a553b184063c4a2685b038b6580
fonseca9001 <joao@fonseca.>
2025-04-18 14:12:44 +0100

    Fix flake output for nixosConfigurations

--- flake.nix
+++ flake.nix
@@ -3,20 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
- };
+  };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      in {
-        nixosConfigurations.moonlight-box = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./hosts/moonlight.nix ];
-        };
-      });
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.moonbox = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./hosts/moonlight.nix ];
+    };
+  };
 }
